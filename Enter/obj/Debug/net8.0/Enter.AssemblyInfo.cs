70a79419a5e3f15b793760cfad2ebb551e38deb4
Wyatt Black <black.1172@buckeyemail.osu.edu>
2025-09-17 15:51:34 -0400

    more folders

--- Enter/obj/Debug/net8.0/Enter.AssemblyInfo.cs
+++ Enter/obj/Debug/net8.0/Enter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd4c311d6abb2c0e8a750194d03198b0aee83e17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdc6846ea9240b75c27bfb810c46a2f1adfa26b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
