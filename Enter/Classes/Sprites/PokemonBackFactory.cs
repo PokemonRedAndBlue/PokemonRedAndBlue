02dac363ce5943d3ced3c8836c80497c69d2be0b
Wyatt Black <black.1172@buckeyemail.osu.edu>
2025-09-18 16:09:26 -0400

    added fromt and back sprite factory

--- Enter/Classes/Sprites/PokemonBackFactory.cs
+++ Enter/Classes/Sprites/PokemonBackFactory.cs
@@ -1,8 +1,43 @@
 using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
 
 namespace MonoGameLibrary.Graphics;
 
-public class PokemonFrontFactory : Sprite
+public class PokemonBackFactory
 {
+    private static PokemonBackFactory _instance;
+    private ContentManager _content;
+
+    // Singleton Instance
+    public static PokemonBackFactory Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new PokemonBackFactory();
+            }
+            return _instance;
+        }
+    }
+
+    private PokemonBackFactory() { }
+
+    public void LoadAllTextures(ContentManager content)
+    {
+        _content = content;
+
+        // Example: Load your textures here
+        // ExampleTexture = _content.Load<Texture2D>("Sprites/ExampleSprite");
+    }
+
+    // Example factory methods
+
+    public Sprite CreateAnimatedSprite()
+    {
+        // return new AnimatedSprite(ExampleTexture);
+        return null;
+    }
 }
--- Enter/Classes/Sprites/PokemonFrontFactory.cs
+++ Enter/Classes/Sprites/PokemonFrontFactory.cs
@@ -1,8 +1,43 @@
 using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
 
 namespace MonoGameLibrary.Graphics;
 
-public class PokemonBackFactory : AnimatedSprite
+public class PokemonFrontFactory
 {
+    private static PokemonFrontFactory _instance;
+    private ContentManager _content;
+
+    // Singleton Instance
+    public static PokemonFrontFactory Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new PokemonFrontFactory();
+            }
+            return _instance;
+        }
+    }
+
+    private PokemonFrontFactory() { }
+
+    public void LoadAllTextures(ContentManager content)
+    {
+        _content = content;
+
+        // Example: Load your textures here
+        // ExampleTexture = _content.Load<Texture2D>("Sprites/ExampleSprite");
+    }
+
+    // Example factory methods
+
+    public Sprite CreateStaticSprite()
+    {
+        // return new StaticSprite(ExampleTexture);
+        return null;
+    }
 }
