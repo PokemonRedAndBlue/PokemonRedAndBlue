4f7e5e9b10ed03cea667cf1f904d35853e65fa13
Wyatt Black <black.1172@buckeyemail.osu.edu>
2025-09-19 14:33:40 -0400

    skeleton for action classes front and back

--- Enter/Classes/GameState/BackStateAction.cs
+++ Enter/Classes/GameState/BackStateAction.cs
@@ -7,4 +7,25 @@ namespace MonoGameLibrary.Graphics;
 
 public class BackStateAction
 {
+    public void IdleBackAction()
+    {
+        // Implement idle back action logic here
+        
+
+    }
+
+    public void AttackBackAction()
+    {
+        // Implement attack back action logic here
+    }
+
+    public void HurtBackAction()
+    {
+        // Implement hurt back action logic here
+    }
+
+    public void DeathBackAction()
+    {
+        // Implement death back action logic here
+    }
 }
--- Enter/Classes/GameState/FrontStateAction.cs
+++ Enter/Classes/GameState/FrontStateAction.cs
@@ -7,4 +7,23 @@ namespace MonoGameLibrary.Graphics;
 
 public class FrontStateAction
 {
+    public void IdleFrontAction()
+    {
+        // Implement idle front action logic here
+    }
+
+    public void AttackFrontAction()
+    {
+        // Implement attack front action logic here
+    }
+
+    public void HurtFrontAction()
+    {
+        // Implement hurt front action logic here
+    }
+
+    public void DeathFrontAction()
+    {
+        // Implement death front action logic here
+    }
 }
