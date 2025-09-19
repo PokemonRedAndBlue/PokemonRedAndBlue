333b6f12eff2334c33402fc18e72db2f1a617cee
black1172 <black.1172@buckeyemail.osu.edu>
2025-09-19 12:59:32 -0400

    added classes and interface for game input

---
+++ Enter/Interfaces/IController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameFile;
+using Microsoft.Xna.Framework;
+
+namespace IController
+{
+    public interface IController
+    {
+        void Update(Game1 game);
+    }
+}
