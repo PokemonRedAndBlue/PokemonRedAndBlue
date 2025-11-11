9bf4616804fa96f6b4e260829cebaa33fd66ede9
black1172 <black.1172@buckeyemail.osu.edu>
2025-11-10 19:47:07 -0500

    added battle UI XML and UIFactory class

---
+++ Enter/Classes/Scenes/UI/UIFactory.cs
@@ -0,0 +1,39 @@
+using System;
+using Microsoft.Xna.Framework.Content;
+using Enter.Classes.Textures;
+
+namespace Enter.Classes.Sprites;
+
+public class UIFactory
+{
+    private static UIFactory _instance;
+    private ContentManager _content;
+    private TextureAtlas _UIAtlas;
+
+    // Singleton Instance
+    public static UIFactory Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new UIFactory();
+            }
+            return _instance;
+        }
+    }
+
+    private UIFactory() { }
+
+    public void LoadAllTextures(ContentManager Content)
+    {
+        _content = Content;
+
+        // Example: Load your textures here
+        _UIAtlas = TextureAtlas.FromFile(_content, "Pokemon_BACK.xml");
+    }
+    public Sprite CreateStaticSprite(String spriteName)
+    {
+        return _UIAtlas.CreateSprite(spriteName);
+    }
+}
