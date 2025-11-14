3cb313223e2612a79e71b0347a7cbccc7d3196a4
Wyatt Black <black.1172@buckeyemail.osu.edu>
2025-11-13 20:11:12 -0500

    added pokeball team display UI

--- Enter/Classes/Scenes/UI/BattleUIHelper.cs
+++ Enter/Classes/Scenes/UI/BattleUIHelper.cs
@@ -3,15 +3,21 @@ using Enter.Classes.Textures;
 using Enter.Classes.Sprites;
 using PokemonGame;
 using Microsoft.Xna.Framework.Graphics;
+using System;
+using Enter.Classes.Animations;
 
 public class BattleUIHelper
 {
     static private float _scale = 2.0f;
+    static private Vector2 uiBasePosition = new Vector2(350, 75);
+    private static Vector2 _pokeballStartPosition;
+    static private Vector2 playerPokeballStartPosition = new Vector2(uiBasePosition.X + (88 * _scale), uiBasePosition.Y + (80 * _scale));
+    static private Vector2 enemyPokeballStartPosition = new Vector2(uiBasePosition.X + (24 * _scale), uiBasePosition.Y + (16 * _scale));
     static public Sprite GetPokeballSprite(string status, TextureAtlas battleUIAtlas)
     {
         switch (status)
         {
-            case "alive":
+            case "idle":
                 return new Sprite(battleUIAtlas.GetRegion("pokeball-present"));
             case "dead":
                 return new Sprite(battleUIAtlas.GetRegion("pokeball-dead"));
@@ -20,13 +26,20 @@ public class BattleUIHelper
         }
     }
 
-    static public void drawPokeballSprites(Pokemon[] team, TextureAtlas battleUIAtlas, SpriteBatch spriteBatch)
+    static public void drawPokeballSprites(Pokemon[] team, TextureAtlas battleUIAtlas, SpriteBatch spriteBatch, Boolean isPlayersTeam)
     {
-        Vector2 pokeballStartPosition = new Vector2(50, 400);
+        if(isPlayersTeam)
+        {
+            _pokeballStartPosition = playerPokeballStartPosition;
+        }
+        else
+        {
+            _pokeballStartPosition = enemyPokeballStartPosition;
+        }
         for(int i = 0; i < 6; i++)
         {
             Sprite pokeballSprite = GetPokeballSprite(team[i].StateMachine.CurrentStateName, battleUIAtlas);
-            pokeballSprite.Draw(spriteBatch, Color.White, pokeballStartPosition + new Vector2(i * 20, 0), _scale);
+            pokeballSprite.Draw(spriteBatch, Color.White, _pokeballStartPosition + new Vector2(i * 8, 0), _scale);
         }
     }
 }
